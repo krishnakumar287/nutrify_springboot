3aef7a4d717572ca8691f2e4259e2da110780642
Hariprasad <hariprasadm.22it@kongu.edu>
2025-08-11 14:04:07 +0530

    inital commit

---
+++ NutrifyShop/src/main/java/com/NutrifyShop/NutrifyShop/NutrifyShopApplication.java
@@ -0,0 +1,13 @@
+package com.NutrifyShop.NutrifyShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutrifyShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutrifyShopApplication.class, args);
+	}
+
+}
